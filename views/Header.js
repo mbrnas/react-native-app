da9d419cef81ee7395988a921f900b6a3aae7583
Matija Brnas <mb8828@rit.edu>
2023-02-25 16:55:41 +0100

    First commit

---
+++ views/Header.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+export class Header extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {isLoggedIn: false};
+  }
+
+  toggleUser = () => {
+    this.setState(previousState => {
+      return{isLoggedIn: !previousState.isLoggedIn }
+    });
+  }
+
+  render(){
+    let dipslay = this.state.isLoggedIn ? 'RIT User' : this.props.message;
+    return(
+      <View style={styles.headStyle}>
+      <Text 
+      style = {styles.headText}
+      onPress = {this.toggleUser}>{dipslay}  </  Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+	headText: {
+		textAlign: 'center',
+		color: '#ffffff',
+		fontSize: 20
+	},
+	headStyle: {
+		paddingTop: 80,
+		paddingBottom: 10,
+		paddingRight: 10,
+		backgroundColor: '#35605a'
+	}
+});
---
+++ views/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import {StyleSheet, Text, View} from 'react-native';
+
+import {Header} from './Header.js'
+
+export class Home extends React.Component{
+  render(){
+   return(
+    <View>
+    <Header message = 'Press to login'/>
+    <Text>This is our homepage</Text>
+    <Text>The other content is here</Text>
+    <Text>Other description text is here</Text>
+    </View>
+   );
+  }
+
+  
+}
